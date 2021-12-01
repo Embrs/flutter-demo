9cfbff6cbb0a7461e7d4031300e562b130806bcc
EMBRS <embrs@EMBRSdeiMac.local>
2021-12-01 09:20:10 +0800

    backup

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("About")),
+        appBar: AppBar(title: Text("Home")),
         body: HomePage(),
       ),
     );
