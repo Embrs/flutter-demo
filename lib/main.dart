7b19d6ceaad86a0dd9656d290b71ed86af0e9954
Harry <ddembrs@gmail.com>
2021-12-13 15:38:22 +0800

    ListView builder complete

--- lib/main.dart
+++ lib/main.dart
@@ -9,44 +9,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("${0.1 + 0.2}")),
-      body: Demo(),
+      body: ListViewDemo(),
     );
   }
 }
 
-class Demo extends StatelessWidget {
-  Demo({Key? key}) : super(key: key);
-  final List<Widget> list = [
-    const ListTile(
-      leading: Icon(Icons.event_available, color: Colors.blue),
-      title: Text(
-        "Test List 1",
-        style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-      ),
-      subtitle: Text("Test List 1"),
-    ),
-    const ListTile(
-      leading: Icon(Icons.event_available, color: Colors.blue),
-      title: Text(
-        "Test List 2",
-        style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-      ),
-      subtitle: Text("Test List 1"),
-    ),
-    const ListTile(
-      leading: Icon(Icons.event_available, color: Colors.blue),
-      title: Text(
-        "Test List 3",
-        style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-      ),
-      subtitle: Text("Test List 1"),
-    )
-  ];
+class ListViewDemo extends StatelessWidget {
+  ListViewDemo({Key? key}) : super(key: key);
+  final List<Product> listItems = List<Product>.generate(
+    500,
+    (index) => Product(
+        name: "測試資料 $index",
+        price: "商品價格 $index",
+        icon: const Icon(Icons.attach_money)),
+  );
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: ListView(
-      children: list,
-    ));
+      child: ListView.builder(
+        itemCount: listItems.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: listItems[index].icon,
+            title: Text("${listItems[index].name}"),
+            subtitle: Text("${listItems[index].price}"),
+          );
+        },
+      ),
+    );
   }
+}
+
+class Product {
+  final String? name;
+  final String? price;
+  final Icon? icon;
+
+  Product({this.name, this.price, this.icon});
 }
